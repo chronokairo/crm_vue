62ccf858d3780319d9d9112038c3ee3778cfa648
Riad Rabelo Makarem <96786977+Riadmakarem@users.noreply.github.com>
2025-07-03 15:17:10 -0400

    a

--- src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia'
-
-export const pinia = createPinia()
-
-// Export stores
-export { useAuthStore } from './auth'
-export { useContactsStore } from './contacts'
-export { useDealsStore } from './deals'
-export { useActivitiesStore } from './activities'
